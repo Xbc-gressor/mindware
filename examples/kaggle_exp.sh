4d871236fa6765a511c2f3b31c4f5e2a22020344
Xbc-TOP <2765024335@qq.com>
2024-06-29 00:54:23 +0800

    tmp

--- examples/kaggle_exp.sh
+++ examples/kaggle_exp.sh
@@ -21,7 +21,7 @@
 
 #python ./kaggle_santander_exp.py --Opt cashfe --time_limit 10024 --per_time_limit 1200 --optimizer smac --evaluation holdout
 # python ./kaggle_santander_exp.py --Opt cashfe --time_limit 10024 --per_time_limit 1200 --optimizer smac --evaluation holdout --x_encode normalize
-python ./kaggle_santander_exp.py --Opt cashfe --time_limit 20024 --per_time_limit 4800 --optimizer smac --evaluation cv --x_encode normalize
+python ./kaggle_santander_exp.py --Opt cashfe --time_limit 20024 --per_time_limit 1800 --optimizer smac --evaluation partial_bohb --x_encode normalize
 
-python ./kaggle_porto_exp.py --Opt cashfe --time_limit 10024 --per_time_limit 1200 --optimizer smac --evaluation holdout --x_encode normalize
-python ./kaggle_porto_exp.py --Opt cashfe --time_limit 20024 --per_time_limit 4800 --optimizer smac --evaluation cv --x_encode normalize
+python ./kaggle_porto_exp.py --Opt cashfe --time_limit 20024 --per_time_limit 1800 --optimizer smac --evaluation holdout --x_encode normalize
+python ./kaggle_porto_exp.py --Opt cashfe --time_limit 30024 --per_time_limit 1800 --optimizer smac --evaluation partial_bohb --x_encode normalize
